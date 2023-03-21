9ee45ab8b305b5d8becb97320c84a66dcd9a0b45
Axel Jimenez <axely7@hotmail.com>
2023-03-21 16:31:56 -0600

    authentication

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useAuthStore } from '../../hooks'
 
 export const Navbar = () => {
+
+  const {startLogout, user} = useAuthStore()
+
+
   return (
     <div className='navbar navbar-dark bg-dark mb-4 px-4'>
-        <span className='navbar-brand'><i className='fas fa-calendar-alt'></i> &nbsp; Axel</span>
-        <button className='btn btn-outline-danger'>
+        <span className='navbar-brand'><i className='fas fa-calendar-alt'></i> &nbsp; {user.name}</span>
+        <button onClick={startLogout} className='btn btn-outline-danger'>
             <i className='fas fa-sign-out-alt'></i>
+            &nbsp;
             <span>Salir</span>
         </button>
     </div>
